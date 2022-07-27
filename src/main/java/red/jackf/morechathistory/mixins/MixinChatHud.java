e7a82028571ef2044ac2c80db886628749d7df0f
Jack Pearce <jack@jackf.red>
2022-07-27 21:15:10 +0100

    Update for 1.19.1

--- src/main/java/red/jackf/morechathistory/mixins/MixinChatHud.java
+++ src/main/java/red/jackf/morechathistory/mixins/MixinChatHud.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(ChatHud.class)
 public class MixinChatHud {
 
-    @ModifyConstant(method = "addMessage(Lnet/minecraft/text/Text;IIZ)V", constant = @Constant(intValue = 100), expect = 2)
+    @ModifyConstant(method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;ILnet/minecraft/client/gui/hud/MessageIndicator;Z)V", constant = @Constant(intValue = 100), expect = 2)
     public int morechathistory_changeMaxHistory(int original) {
         return 16384;
     }

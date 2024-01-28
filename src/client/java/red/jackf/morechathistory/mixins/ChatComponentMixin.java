50626bd4a2bfca7b138b13fa3b695ed42332fae0
Jack Pearce <jack@jackf.red>
2024-01-28 15:35:58 +0000

    [internal] formatting

--- src/client/java/red/jackf/morechathistory/mixins/ChatComponentMixin.java
+++ src/client/java/red/jackf/morechathistory/mixins/ChatComponentMixin.java
@@ -8,7 +8,9 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(ChatComponent.class)
 public class ChatComponentMixin {
 
-    @ModifyConstant(method = "addMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/MessageSignature;ILnet/minecraft/client/GuiMessageTag;Z)V", constant = @Constant(intValue = 100), expect = 2)
+    @ModifyConstant(method = "addMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/MessageSignature;ILnet/minecraft/client/GuiMessageTag;Z)V",
+            constant = @Constant(intValue = 100),
+            expect = 2)
     public int morechathistory_changeMaxHistory(int original) {
         return 16384;
     }

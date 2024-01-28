d07736119fd8d78adf101e0e7a12a2be8ec960e8
Jack Pearce <jack@jackf.red>
2024-01-28 15:47:17 +0000

    [internal] reminder comment

--- src/client/java/red/jackf/morechathistory/mixins/ChatComponentMixin.java
+++ src/client/java/red/jackf/morechathistory/mixins/ChatComponentMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(ChatComponent.class)
 public class ChatComponentMixin {
 
+    // todo move to mixin extras ModifyExpressionValue when it's likely most have changed to floader 0.15
     @ModifyConstant(method = "addMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/MessageSignature;ILnet/minecraft/client/GuiMessageTag;Z)V",
             constant = @Constant(intValue = 100),
             expect = 2)
